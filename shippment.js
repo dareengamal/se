fded880353bbbc6da7775ca53d37e2d676541c87
dareengamal <76594921+dareengamal@users.noreply.github.com>
2022-06-09 12:26:35 +0200

    Add files via upload

--- shippment.js
+++ shippment.js
@@ -1,3 +1,9 @@
-export const getProducts = async (req,res) => {
-    res.send('qwe')
-}
+import mongoose from 'mongoose';
+const postSchema=mongoose.Schema({
+    shippment_status:String,
+    order_id:Number,
+    address:String,
+    order_status:String
+});
+const shippment= mongoose.model('shippment',postSchema);
+export default shippment;
